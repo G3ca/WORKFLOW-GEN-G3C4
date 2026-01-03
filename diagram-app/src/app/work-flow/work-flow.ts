021a92648fda87dca583466a7a0a3087ca24da43
G3c4 <geca.dawid21@gmail.com>
2026-01-03 17:07:53 +0100

    Dodanie drag N Roll

--- diagram-app/src/app/work-flow/work-flow.ts
+++ diagram-app/src/app/work-flow/work-flow.ts
@@ -23,28 +23,62 @@ export class WorkFlowComponent {
   @Input() steps: WorkflowStep[] = [];
 
 
+  //Stan Widoku
+  scale = 1;
+  pan = { x: 0, y: 0 };
+
+  //Stan przeciągania
+  isDragging = false;
+  private lastMousePosition = { x: 0, y: 0 };
+  
+  OnWheel(event: WheelEvent) {
+    event.preventDefault();
+    const zoomIntensity = 0.1;
+    this.scale += zoomIntensity * (event.deltaY > 0 ? -1 : 1);
+    this.scale = Math.min(Math.max(this.scale, 0.1), 5); // Ograniczenie skali między 0.1 a 5
+  }
+
+  StartPan(event: MouseEvent) {
+    event.preventDefault();
+    this.isDragging = true;
+    this.lastMousePosition = { x: event.clientX, y: event.clientY };
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  OnMouseMove(event: MouseEvent) {
+    if (this.isDragging) {
+      const deltaY = event.clientY - this.lastMousePosition.y;
+      const deltaX = event.clientX - this.lastMousePosition.x;
+      
+      this.pan.x += deltaX;
+      this.pan.y += deltaY;
+
+      this.lastMousePosition = { x: event.clientX, y: event.clientY };
+    }
+  
+  }
+
+  @HostListener('document:mouseup', ['$event'])
+  OnMouseUp(event: MouseEvent) {
+    this.isDragging = false;
+  }
   
   
   
   
-  
-  
-  
-  
-  
-  
+
   
   
   //Umożliwienie przewijania poziomego za pomocą kółka myszy wygenerowanego diagramu
-  @ViewChild('scrollContainer') scrollContainer!: ElementRef<HTMLElement>;
+  // @ViewChild('scrollContainer') scrollContainer!: ElementRef<HTMLElement>;
 
-  @HostListener('wheel', ['$event'])
-  onWheel(event: WheelEvent) {
-    if (this.scrollContainer) {
-      event.preventDefault();
-      this.scrollContainer.nativeElement.scrollLeft += event.deltaY;
-    }
-  }
+  // @HostListener('wheel', ['$event'])
+  // onWheel(event: WheelEvent) {
+  //   if (this.scrollContainer) {
+  //     event.preventDefault();
+  //     this.scrollContainer.nativeElement.scrollLeft += event.deltaY;
+  //   }
+  // }
 
 }
 
