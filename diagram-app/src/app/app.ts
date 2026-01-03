1246c769c9312337fc9f8429382cf8d62e92ca90
G3c4 <geca.dawid21@gmail.com>
2026-01-03 20:16:47 +0100

    Dodanie zapisu, odczytu oraz zapisu diagramu do formatu svg

--- diagram-app/src/app/app.ts
+++ diagram-app/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { WorkFlowComponent, WorkflowStep } from './work-flow/work-flow';
+import * as htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'app-root',
@@ -56,5 +57,67 @@ export class AppComponent {
     this.rect1 = '';
     this.rect2 = '';
     this.rect3 = '';
+  }
+
+
+//Zapis diagramu do pliku JSON
+
+saveWorkflow() {
+    const dataStr = JSON.stringify(this.workflowSteps, null, 2);
+    const blob = new Blob([dataStr], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `workflow-diagram-${Date.now()}.json`;
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+}
+
+
+loadWorkflow(event: any) {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      try {
+        const content = e.target?.result;
+        const loadedSteps: WorkflowStep[] = JSON.parse(content);
+        if (!Array.isArray(loadedSteps)) {
+          alert('Błąd: Niepoprawny format pliku!');
+          throw new Error('Nieprawidłowy format pliku JSON.');
+        }
+        this.workflowSteps = loadedSteps;
+
+      } catch (error) {
+        console.error('Błąd podczas wczytywania pliku:', error);
+        alert('Błąd odczytu pliku JSON.');
+      }
+    };
+    reader.readAsText(file);
+    event.target.value = ''; // Reset input file value
+  }
+
+
+  exportToSVG() {
+    const node = document.getElementById('workflow-container');
+    if (!node){
+      alert('Nie znaleziono kontenera diagramu!');
+      return;
+    }
+
+    htmlToImage.toSvg(node)
+    .then((dataUrl: string) => {
+      const a = document.createElement('a');
+      a.href = dataUrl;
+      a.download = `workflow-diagram-${Date.now()}.svg`;
+      a.click();
+    })
+    .catch((error: any) => {
+      console.error('Błąd podczas eksportu do SVG:', error);
+      alert('Wystąpił błąd podczas eksportu do SVG.');
+    });
   }
 }
